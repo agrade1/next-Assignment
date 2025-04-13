43cdfe256ea24421a6e07d9b31f3db097c9ff3d9
agrade1 <kigf59@gmail.com>
2025-04-14 05:54:27 +0900

    about-us 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,14 @@
+import styles from "../../styles/About.module.css"
+
 export const metadata = {
     title: "About",
 }
 export default function homePage() {
     return (
         <div>
-            <h2>About us</h2>
+            <h2 className={styles.title}>About us</h2>
+            <p className={styles.text}>Welcome to the official explorer for The New York Times Best Seller list explorer. </p>
+            <p className={styles.text}>We hope you enjoy your stay!</p>
         </div>
     )
 }
